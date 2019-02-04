a622c999cf3ae29ade916bde41e9a61852bc2867
joshua-miles <joshua@christetulit.org>
2019-02-04 17:30:02 -0600

    minor updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,9 +7,7 @@ class Nav extends React.Component {
 			<div className="navWrapper">
 				<span className="headerText">Hogwarts</span>
 				<div className="TwirlyPig">
-					<a href="https://www.lowes.com/pd/LG-24-7-cu-ft-French-Door-Refrigerator-with-Ice-Maker-Stainless-steel/4746231">
-						<img src={piggy} className="App-logo" alt="piggy" />
-					</a>
+					<img src={piggy} className="App-logo" alt="piggy" />
 				</div>
 				<span className="normalText">A React App for County Fair Hog Fans</span>
 			</div>
